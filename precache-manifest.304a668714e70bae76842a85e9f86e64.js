814609cc91fac716c9f615637bb41cbf25d7b8af
Wqk <276368738@qq.com>
2020-06-23 23:02:26 +0800

    first commit

---
+++ precache-manifest.304a668714e70bae76842a85e9f86e64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a263839dc33bb58ec4a",
+    "url": "css/app.2ecfad89.css"
+  },
+  {
+    "revision": "1f0ec1792ed20b019fc2187cd6e52174",
+    "url": "img/todolist_sprite.1f0ec179.png"
+  },
+  {
+    "revision": "130e69cc7aa080dfc80d0908c8e65241",
+    "url": "index.html"
+  },
+  {
+    "revision": "4a263839dc33bb58ec4a",
+    "url": "js/app.e38f8953.js"
+  },
+  {
+    "revision": "47cf6baaf67df72984dc",
+    "url": "js/chunk-vendors.d180f2c9.js"
+  },
+  {
+    "revision": "8d4192f82ff22aae2f49028b915656fa",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
